73762d2f6a4ce538596b0ec1a26319dcd9f53a8e
Harry Stringa <hwstringer@waimea.school.nz>
2025-05-09 15:06:34 +1200

    start

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return render_template("pages/home.jinja")
+
+@app.get("/test/")
+def test():
+    return render_template("pages/test.jinja")
